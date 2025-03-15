7965e3c1504466088d6fff26f7599731f2fb4af6
Nikita <nick1tapinyagin@yandex.ru>
2025-03-15 06:30:11 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -12,9 +12,9 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Здравствуйте!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
+def echo(update: Update, context: CallbackContext, project_id) -> None:
     """Echo the user message."""
-    message, is_fallback = detect_intent_texts(os.environ["PROJECT_ID"],
+    message, is_fallback = detect_intent_texts(project_id,
                                                f'tg-{update.message.chat_id}',
                                                update.message.text, 'en')
     update.message.reply_text(message)
@@ -26,8 +26,12 @@ def main():
     updater = Updater(token=telegram_token, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,
-                                          echo))
+    dispatcher.add_handler(
+        MessageHandler(
+            Filters.text & ~Filters.command,
+            lambda update, context: echo(update, context, os.environ["PROJECT_ID"])
+        )
+    )
     updater.start_polling()
 
 
